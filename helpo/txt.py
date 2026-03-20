9a9bf57bcbe9e2edd84cca4e05a87cde27c4a53d
YAMOVIES <ya753121988@gmail.com>
2026-03-20 17:32:08 +0600

    Add files via upload

---
+++ helpo/txt.py
@@ -0,0 +1,17 @@
+
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+  
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """<blockquote><b><u>ᴏᴡɴᴇʀ & ᴅᴇᴠᴇʟᴏᴘᴇʀꜱ</b></u></blockquote>
+• ᴏᴡɴᴇʀ : <a href=https://t.me/--->👑 -- 👑</a>
+• ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/LazyDeveloperr>🍟• ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀʀ •❤</a>
+"""
---
+++ helpo/youtube.py
@@ -0,0 +1,43 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+def get_youtube_video_id(url):
+    """Extracts the video ID from a YouTube URL"""
+    video_id = None
+    if 'youtube.com' in url:
+        video_id = url.split('v=')[-1]
+        if '&' in video_id:
+            video_id = video_id.split('&')[0]
+    elif 'youtu.be' in url:
+        video_id = url.split('/')[-1]
+    return video_id
+
+def get_resolution_keyboard(video):
+    resolutions = {}
+    for stream in video.streams.filter(progressive=True):
+        resolution = stream.resolution or "Audio Only"
+        resolutions[resolution] = stream.itag
+
+    keyboard = []
+    for resolution, itag in resolutions.items():
+        callback_data = f"res_{itag}"
+        button = InlineKeyboardButton(resolution, callback_data=callback_data)
+        keyboard.append([button])
+
+    return InlineKeyboardMarkup(keyboard)
+
+def get_filetype_keyboard(video, resolution):
+    stream = video.streams.filter(res=f"{resolution}p", file_extension="mp4").first()
+    filetypes = ["mp4", "webm"]
+    if stream and stream.is_progressive:
+        filetypes.append("3gp")
+
+    buttons = [
+        InlineKeyboardButton(filetype.upper(), callback_data=f"filetype_{filetype}")
+        for filetype in filetypes
+    ]
+    keyboard = InlineKeyboardMarkup([buttons])
+
+    return keyboard
+
+async def progress(current, total):
+    print(f"{current * 100 / total:.1f}%")
